c317a8075758bdf7c12147e48f7f64a0997ffc78
matutaj <matutajorge2002@gmail.com>
2024-03-05 09:41:55 +0000

    feat:add styled-component with router-dom

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Menu, Item } from "./style";
+function NavBar(){
+    return(
+        <>
+        <Menu>
+            <Item>
+
+            </Item>
+        </Menu>
+        </>
+    )
+}
+
+export default NavBar;
---
+++ src/components/style.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Menu = styled.div``
+
+export const Item = styled.li``
