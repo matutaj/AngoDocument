2515b174179b62a797e39f31c0df4ab3e9779c27
matutaj <matutajorge2002@gmail.com>
2024-03-07 09:49:55 +0000

    feat: add banner components

---
+++ src/components/banner.tsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import {BackgroundBanner} from "./style"
+import Button from "./button"
+interface IDataBanner{
+title: string,
+text:string
+
+}
+
+function Banner( {title, text}:IDataBanner){
+    return(
+        <BackgroundBanner>
+            <h1>{title}</h1>
+            <p>{text}</p>
+            <br/>
+            <Button title="Agendar Agora!" />
+        </BackgroundBanner>
+    )
+}
+export default Banner;
